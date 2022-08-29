161e315b16597f013cdaac8494c410b640df1fb9
duckyprogramming2022 <gueney.timur@outlook.de>
2022-08-29 12:10:33 +0200

    formatted code to look cleaner

--- js.js
+++ js.js
@@ -16,16 +16,16 @@ function createEtchPad() {
 
 function  defBoxSize(paras) {
         paras.forEach((para) => {
-        console.log(para);
-        para.style.boxSizing = "border-box";
-        para.style.width = `${960/16}px`;
-        para.style.height = `${960/16}px`;
-    });
+            console.log(para);
+            para.style.boxSizing = "border-box";
+            para.style.width = `${960/16}px`;
+            para.style.height = `${960/16}px`;
+        });
 };
 
 function defBoxStyle(paras) {
         paras.forEach((para) => {
-        para.style.margin = "0px";
-        para.style.border = "1px solid lightgrey";
-    });
+            para.style.margin = "0px";
+            para.style.border = "1px solid lightgrey";
+        });
 };
