93f60cd9139356da9da66d14a1f075c4e697286a
duckyprogramming2022 <gueney.timur@outlook.de>
2022-08-29 18:25:44 +0200

    finished

--- js.js
+++ js.js
@@ -1,33 +1,36 @@
 const etchPad = document.querySelector('.etchpad');
 const input = document.querySelector('input');
-
-let gridSize = praseInt(
-    input.addEventListener('keydown', function(e){
-        if (e.key === 'Enter') {
-            return e.target.value;
-        };
-    });
-)
+const btn1 = document.querySelector('#create')
 
 function createEtchPad() {
+    if (etchPad.childNodes.length > 0) {
+        while (etchPad.firstChild) {
+            etchPad.removeChild(etchPad.firstChild);
+        }
+    } 
+    let gridCount = input.value;
     let rows = 1;
-        for (rows; rows <=16*16; rows++) {
-            const para = document.createElement('p');
-            etchPad.appendChild(para);
-        };
-    const paras = document.querySelectorAll('p')
-    return paras;
-};
+    for (rows; rows <=gridCount*gridCount; rows++) {
+        const para = document.createElement('p');
+        etchPad.appendChild(para);
+    };
+    defBoxSizeStyle()
+    };
 
 function  defBoxSizeStyle() {
+    let gridCount = input.value;
+    let gridSize = 960/gridCount;
     etchPad.style.border = "2px solid black";
     etchPad.childNodes.forEach((para) => {
-            console.log(para);
             para.style.boxSizing = "border-box";
-            para.style.width = `${960/16}px`;
-            para.style.height = `${960/16}px`;
+            para.style.width = `${gridSize}px`;
+            para.style.height = `${gridSize}px`;
             para.style.margin = "0px";
             para.style.border = "1px solid lightgrey";
             para.addEventListener(('mouseover'), () => para.style.backgroundColor = "red");
         });
 };
+
+function restart() {
+    etchPad.childNodes.forEach(para => para.style.backgroundColor = "white");
+}
